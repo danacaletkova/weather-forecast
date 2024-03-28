ffc38562cfc6041f462ee1a1af8e9d185be0ad17
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-03-28 15:24:06 +0800

    Improved search engine using API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let temperatureElement = document.querySelector("#temperature");
+  let currentTemperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(currentTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "5o168182b8atd0a481fedf7024b43479";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#city-search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Perth");
