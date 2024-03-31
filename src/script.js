457c4eaf08c2049277fbd87f11e7973c25f8dc2b
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-03-31 16:19:55 +0800

    Injected time data

--- src/script.js
+++ src/script.js
@@ -1,4 +1,33 @@
+function formatDate(date) {
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let weekday = weekdays[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${weekday}, ${hours}:${minutes}`;
+}
+
 function refreshWeather(response) {
+  let currentTime = new Date(response.data.time * 1000);
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate(currentTime);
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
 
